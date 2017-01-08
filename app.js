ad7c704d5a9c8e986afd512aac1e6eb913d95622
Mikita Lisavets <mikita.lisavets@gmail.com>
2017-01-08 20:23:01 +0300

    Make form for connect to bluetooth device

--- app.js
+++ app.js
@@ -1,34 +1,71 @@
 let APP = ((window, document) => {
-  let serviceId, characteristicId;
-
-  let getBluetoothOptions = () => {
-    serviceId = document.getElementById('serviceId').value;
-    characteristicId = document.getElementById('characteristicId').value;
-  }
-
-  let handleCharacteristicData = (characteristic) => {
-    if (characteristic === null) {
-      console.log("Characteristic isn't available");
-      return Promise.resolve();
-    }
-    return characteristic
-      .readValue()
-      .then(characteristicData => console.log(characteristicData));
-  }
-
-  let onSubmit = (event) => {
-    event.preventDefault();
-    getBluetoothOptions();
+  let deviceName, serviceUuid, characteristicUuid, controlType, characteristicValue,
+    currentService, currentCharacteristic;
+  
+  function connect() {
+    getOptions();
     navigator.bluetooth.requestDevice({
-      filters: [{services: [serviceId]}],
-      optionalServices: [serviceId]
+      filters: [{name: deviceName}],
+      optionalServices: [serviceUuid]
     }).then(device => device.gatt.connect())
-    .then(server => server.getPrimaryService(serviceId))
-    .then(service => service.getCharacteristic(characteristicId))
-    .then(handleCharacteristicData);
+    .then(server => server.getPrimaryService(serviceUuid))
+    .then(service => {
+      currentService = service;
+      return service.getCharacteristic(characteristicUuid)
+    })
+    .then(characteristic => {
+      currentCharacteristic = characteristic;
+      onSuccessConnect();
+    });
+  };
+
+  function onSuccessConnect() {
+    alert('Device connected');
+  };
+
+  function getOptions() {
+    let controlTypes;
+    deviceName = document.getElementById('deviceName').value;
+    
+    serviceUuid = document.getElementById('serviceUuid').value;
+    if (serviceUuid.indexOf('0x') === 0) serviceUuid = parseInt(serviceUuid, 16);
+    
+    characteristicUuid = document.getElementById('characteristicUuid').value;
+    if (characteristicUuid.indexOf('0x') === 0) characteristicUuid = parseInt(characteristicUuid, 16);
+    
+    characteristicValue = document.getElementById('characteristicValue');
+
+    controlTypes = document.getElementsByName('controlType');
+    controlTypes.forEach(elem => {
+      if (elem.checked) controlType = elem.value;
+    });
+  }
+
+  function handleWriteCharacteristicData(characteristicValue) {
+    if (!currentCharacteristic) return;
+    currentCharacteristic.writeValue(new Uint8Array([parseInt(characteristicValue.value, 16)]));
+  }
+
+  function handleReadCharacteristicData() {
+    if (!currentCharacteristic) return;
+    return currentCharacteristic.readValue()
+      .then(characteristicData => {
+        console.log(characteristicData);
+        characteristicValue.value = characteristicData;
+      });
+  }
+
+  function submit() {
+    getOptions();
+    if (+controlType) {
+      handleWriteCharacteristicData(characteristicValue);
+    } else {
+      handleReadCharacteristicData();
+    }
   }
 
   return {
-    onSubmit: onSubmit
+    connect: connect,
+    submit: submit
   };
 })(window, document)
